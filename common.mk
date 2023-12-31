90f6829e1bc2419cb9d8acec26d829058b856a67
Pranav Vashi <neobuddy89@gmail.com>
2024-01-01 01:09:09 +0530

    sm8150-common: Link dolby blobs with v33 libstagefright_foundation
    Change-Id: Ic5084220aa15f00630dcd4030e7dc37c67486bc3
    Signed-off-by: mukesh22584 <mks22584@gmail.com>

--- common.mk
+++ common.mk
@@ -327,6 +327,10 @@ TARGET_EXCLUDES_AUDIOFX := true
 PRODUCT_PACKAGES += \
     XiaomiDolby
 
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v33/arm/arch-arm-armv7-a-neon/shared/vndk-core/libstagefright_foundation.so:$(TARGET_COPY_OUT_VENDOR)/lib/libstagefright_foundation-v33.so \
+    prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-core/libstagefright_foundation.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libstagefright_foundation-v33.so
+
 # Overlays
 $(call inherit-product, hardware/oplus/overlay/generic/generic.mk)
 $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
