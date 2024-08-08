c3dc8c797c70e04c73436195490218ca99f6b667
LuK1337 <priv.luk@gmail.com>
2024-08-09 00:26:02 +0200

    sm8150-common: Build wvmkiller
    Change-Id: I1505f82dfa92933f2a2fcf5f4b3c96ce4f17c4fb
    Signed-off-by: mukesh22584 <mks22584@gmail.com>

--- common.mk
+++ common.mk
@@ -161,7 +161,8 @@ PRODUCT_COPY_FILES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
-    android.hardware.drm-service.clearkey
+    android.hardware.drm-service.clearkey \
+    wvmkiller
 
 # fastbootd
 PRODUCT_PACKAGES += \
