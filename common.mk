4c48c83e31d34f20e9e4fa1213c350e286c671c1
ShevT <evgeny.shishkov@gmail.com>
2024-08-30 12:40:58 +0300

    sm8150-common: Building DeviceAsWebcam via the option
    Signed-off-by: mukesh22584 <mks22584@gmail.com>

--- common.mk
+++ common.mk
@@ -119,6 +119,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+TARGET_BUILD_DEVICE_AS_WEBCAM := true
+
 # Configstore
 PRODUCT_PACKAGES += \
     disable_configstore
